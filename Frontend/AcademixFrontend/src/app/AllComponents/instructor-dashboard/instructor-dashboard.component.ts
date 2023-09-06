40cb32b828f41e447ad1edbdd5758630787c31be
Nitish Kumar <kumar.nit1021@gmail.com>
2023-09-06 19:27:48 +0530

    instructor add, login , CRUD course, partial -  assignment

---
+++ Frontend/AcademixFrontend/src/app/AllComponents/instructor-dashboard/instructor-dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+
+@Component({
+  selector: 'app-instructor-dashboard',
+  templateUrl: './instructor-dashboard.component.html',
+  styleUrls: ['./instructor-dashboard.component.css']
+})
+export class InstructorDashboardComponent implements OnInit{
+
+  user !: string | null;
+
+  loggedInUser !: object;
+    constructor(private authService: AuthService, private router: Router){}
+
+    ngOnInit(): void {
+      this.user = localStorage.getItem("user"); 
+      this.loggedInUser = this.authService.getUser()
+    }
+
+}
